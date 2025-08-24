a1c567abdcee47756936ca351f95720b618b86cb
shusaku009 <shusaku009@gmail.com>
2025-08-24 23:53:26 +0900

    add: clickイベントで要素を追加する

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,24 @@ function onClickAdd(): void {
   (document.getElementById("add-text") as HTMLInputElement).value = "";
 
   const li = document.createElement("li");
-  console.log(li);
 
   const div = document.createElement("div");
   div.className = "list-row";
 
   const p = document.createElement("p");
+  p.className = "todo-item";
   p.innerText = inputText;
+
+  div.appendChild(p);
+  li.appendChild(div);
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
     alert("完了");
   });
+
+  document.getElementById("incompleted-list")?.appendChild(li);
 }
 
 document.getElementById("add-button")?.addEventListener("click", onClickAdd);
