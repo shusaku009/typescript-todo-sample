ac7fee350563da30687f7dfb9a0688fa775bc8cc
shusaku009 <shusaku009@gmail.com>
2025-09-07 19:23:14 +0900

    clean: 重複しているコードの削除と、変数をグローバルに定義

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,24 @@
+const incompletedList = document.getElementById("incompleted-list");
+const completeList = document.getElementById("complete-list");
+const completeButton = document.createElement("button");
+const div = document.createElement("div");
+
 function onClickAdd(): void {
   const inputText: string = (
     document.getElementById("add-text") as HTMLInputElement
   ).value;
   (document.getElementById("add-text") as HTMLInputElement).value = "";
 
-  const incompletedList = document.getElementById("incompleted-list");
-  const completeList = document.getElementById("complete-list");
   if (!incompletedList || !completeList) return;
-  const div = document.createElement("div");
   const ul = document.createElement("ul");
   const li = document.createElement("li");
   div.className = "task-button__flex";
 
   li.innerText = inputText;
 
-  const completeButton = document.createElement("button");
   completeButton.id = "complete";
   completeButton.innerText = "完了";
   completeButton.type = "button";
-  completeButton.addEventListener("click", () => {
-    const moveTarget = completeButton.closest("div");
-    if (!moveTarget || !completeButton || !completeButton.nextElementSibling)
-      return;
-    completeButton.nextElementSibling.remove();
-    completeButton.remove();
-    const returnButton = document.createElement("button");
-    returnButton.innerText = "戻す";
-    returnButton.id = "return-button";
-    div.appendChild(returnButton);
-    completeList.appendChild(moveTarget);
-  });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
