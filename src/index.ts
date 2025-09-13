2332ada4180c5c69f6d1a0e8b162b5edf940f335
shusaku009 <shusaku009@gmail.com>
2025-09-13 18:28:35 +0900

    fix: 関数分割でDOMの参照が初期化されてしまい、意図した挙動にならない問題を修正

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-const incompletedList = document.getElementById("incompleted-list");
-const completeList = document.getElementById("complete-list");
-const completeButton = document.createElement("button");
-const div = document.createElement("div");
-
 function onClickAdd(): void {
+  const incompletedList = document.getElementById("incompleted-list");
+  const completeList = document.getElementById("complete-list");
+  const completeButton = document.createElement("button");
+  const returnButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
+  const div = document.createElement("div");
   const inputText: string = (
     document.getElementById("add-text") as HTMLInputElement
   ).value;
   (document.getElementById("add-text") as HTMLInputElement).value = "";
 
-  if (!incompletedList || !completeList) return;
+  completeButton.id = "complete";
+  completeButton.innerText = "完了";
+  completeButton.type = "button";
+
+  deleteButton.innerText = "削除";
+  deleteButton.type = "button";
+
   const ul = document.createElement("ul");
   const li = document.createElement("li");
   div.className = "task-button__flex";
 
   li.innerText = inputText;
 
-  completeButton.id = "complete";
-  completeButton.innerText = "完了";
-  completeButton.type = "button";
-
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "削除";
-  deleteButton.type = "button";
   deleteButton.addEventListener("click", () => {
+    if (!incompletedList) return;
     const deleteTarget = deleteButton.closest("div");
+
     if (!deleteTarget) return;
     incompletedList.removeChild(deleteTarget);
   });
@@ -34,10 +36,6 @@ function onClickAdd(): void {
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
 
-  incompletedList?.appendChild(div);
-}
-
-function addCompleteButton() {
   const moveTarget = completeButton.closest("div");
 
   if (
@@ -47,14 +45,20 @@ function addCompleteButton() {
     !completeList
   )
     return;
-  completeButton.nextElementSibling.remove();
-  completeButton.remove();
-  const returnButton = document.createElement("button");
-  returnButton.innerText = "戻す";
-  returnButton.id = "return-button";
-  div.appendChild(returnButton);
-  completeList.appendChild(moveTarget);
+
+  completeButton.addEventListener("click", () => {
+    if (!completeButton.nextElementSibling) return;
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    returnButton.innerText = "戻す";
+    returnButton.id = "return-button";
+    div.appendChild(returnButton);
+    completeList.appendChild(moveTarget);
+  });
+
+  incompletedList?.appendChild(moveTarget);
+
+  incompletedList?.appendChild(div);
 }
 
 document.getElementById("add-button")?.addEventListener("click", onClickAdd);
-completeButton.addEventListener("click", addCompleteButton);
